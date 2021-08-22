e546b1e8a7b1fc5b6965b04ff2aac06131dd4d81
Jubair-saifi <jubair.saifi.cs.2017@miet.ac.in>
2021-08-23 02:43:04 +0530

    commit into tasks part

--- src/component/task.jsx
+++ src/component/task.jsx
@@ -10,6 +10,17 @@ import React from 'react';
         <span className="add-task-text">Add task</span>
        </div>
 
+       <div className="add-task-dialog">
+       <input/>
+       <div className="add-task-action-container">
+         <div className="btns-container">
+           <button className="add-btn">Add task</button>
+           <button className="cancel-btn">Cancel</button>
+         </div>
+         <div className="icon-container"></div>
+       </div>
+       </div>
+
      </div>
    )
  }
